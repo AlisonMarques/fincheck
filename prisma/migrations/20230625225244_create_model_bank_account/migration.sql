1b3f54a4c381c9f4abae41d9897f5d5b12d4a0eb
Alison Marques <alison-marques@outlook.com.br>
2023-06-25 22:29:43 -0300

    Aula 3

---
+++ prisma/migrations/20230625225244_create_model_bank_account/migration.sql
@@ -0,0 +1,15 @@
+-- CreateEnum
+CREATE TYPE "bank_account_type" AS ENUM ('CHECKING', 'INVESTMENT', 'CASH');
+
+-- CreateTable
+CREATE TABLE "bank_accounts" (
+    "id" UUID NOT NULL,
+    "userId" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "initial_balance" DOUBLE PRECISION NOT NULL,
+    "type" "bank_account_type" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "bank_accounts_pkey" PRIMARY KEY ("id")
+);
